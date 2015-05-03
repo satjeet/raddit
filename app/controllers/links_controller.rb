6053579db0a5050da895c3baad2d66ebc4614510
arturo <arturo.alfaro87@gmail.com>
2015-05-03 20:56:56 -0300

    autorizacion de links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,12 +1,17 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
-
+  before_action :authorized_user, only: [:edit, :update, :destroy]
+  
+  before_filter :authenticate_user!, :except => [:index, :show]
   # GET /links
   # GET /links.json
   def index
     @links = Link.all
   end
-
+def authorized_user
+  @link = current_user.links.find_by(id: params[:id])
+  redirect_to links_path, notice: "Not authorized to edit this link" if @link.nil?
+end
   # GET /links/1
   # GET /links/1.json
   def show
@@ -14,7 +19,7 @@ class LinksController < ApplicationController
 
   # GET /links/new
   def new
-    @link = Link.new
+    @link = current_user.links.build
   end
 
   # GET /links/1/edit
@@ -24,8 +29,8 @@ class LinksController < ApplicationController
   # POST /links
   # POST /links.json
   def create
-    @link = Link.new(link_params)
-
+    #@link = Link.new(link_params)
+     @link = current_user.links.build(link_params)
     respond_to do |format|
       if @link.save
         format.html { redirect_to @link, notice: 'Link was successfully created.' }
